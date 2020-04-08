f1280d864768a4f3c613086c55b49c7101b949f5
gabri <gabri@DESKTOP-75KHAGD>
2020-04-08 20:33:28 -0300

    feat: Criação da classe "Equipamento"

--- src/br/com/connect/main/LeilaoOnline.java
+++ src/br/com/connect/main/LeilaoOnline.java
@@ -2,11 +2,11 @@ package br.com.connect.main;
 
 public class LeilaoOnline {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+          
+        
     }
     
 }
---
+++ src/br/com/connect/modelo/Equipamento.java
@@ -0,0 +1,5 @@
+package br.com.connect.modelo;
+
+public class Equipamento {
+    
+}

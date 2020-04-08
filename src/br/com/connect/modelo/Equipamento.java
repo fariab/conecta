e88df96a0950290f4106721cdff5216d1ecffa97
gabri <gabri@DESKTOP-75KHAGD>
2020-04-08 20:53:14 -0300

    feat: Ajustes da classe "Equipamento"/Criação de aributos iniciais da classe "Equipamento"

--- src/br/com/connect/modelo/Equipamento.java
+++ src/br/com/connect/modelo/Equipamento.java
@@ -1,5 +1,14 @@
 package br.com.connect.modelo;
 
 public class Equipamento {
+
+    Integer idCadastroEquipamento;
+    String nomeEquipamento;
+    Integer numeroDeSerie;
+    String fabricanteEquipamento;
+    Float valorBaseHora;
     
+    
+    
+
 }

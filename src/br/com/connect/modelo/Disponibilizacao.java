2db83fde635d9d655c35d4ab08e5618bba557702
gustavovete <>
2020-04-08 21:35:48 -0300

    feat: Classe Disponibilidade - Criação atributo ID. - HashCode ID. - Equals ID.

--- src/br/com/connect/modelo/Disponibilizacao.java
+++ src/br/com/connect/modelo/Disponibilizacao.java
@@ -15,12 +15,39 @@ public class Disponibilizacao {
 
     @Override
     public String toString() {
-        return "Disponibilizacao{" + "DataDisponibilizacao=" + DataDisponibilizacao + ", DataDevolucao=" + DataDevolucao + ", valor=" + valor + ", ConfirmacaoDevolucao=" + ConfirmacaoDevolucao + '}';
+        return "Disponibilizacao{" + "DataDisponibilizacao=" + getDataDisponibilizacao() + ", DataDevolucao=" + getDataDevolucao() + ", valor=" + getValor() + ", ConfirmacaoDevolucao=" + getConfirmacaoDevolucao() + '}';
     }
     private Date DataDisponibilizacao;
     private Date DataDevolucao;
     private float valor;
     private String ConfirmacaoDevolucao;
+    private int ID;
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + this.ID;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Disponibilizacao other = (Disponibilizacao) obj;
+        if (this.ID != other.ID) {
+            return false;
+        }
+        return true;
+    }
+    
 
     /**
      * @return the DataDisponibilizacao
@@ -76,5 +103,19 @@ public class Disponibilizacao {
      */
     public void setConfirmacaoDevolucao(String ConfirmacaoDevolucao) {
         this.ConfirmacaoDevolucao = ConfirmacaoDevolucao;
+    }
+
+    /**
+     * @return the ID
+     */
+    public int getID() {
+        return ID;
+    }
+
+    /**
+     * @param ID the ID to set
+     */
+    public void setID(int ID) {
+        this.ID = ID;
     }
 }

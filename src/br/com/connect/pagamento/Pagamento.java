1aabb5f422d6f57cf8db4e6e756dc7e198d0c09c
Lucas Miguel <Lucas Miguel@DESKTOP-U7LANCM>
2020-05-15 19:29:03 -0300

    feat: Criação da classe pagamento

---
+++ src/br/com/connect/pagamento/Pagamento.java
@@ -0,0 +1,15 @@
+
+package br.com.connect.pagamento;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Lucas Miguel
+ */
+public class Pagamento {
+    nteger idPagamento;
+    String tipoPagamento;
+    Integer idUsuarioFk;
+  
+}

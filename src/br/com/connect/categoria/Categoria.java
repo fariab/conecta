0f8847860fa2e40194f95b44b92cff0305d12be4
Lucas Miguel <Lucas Miguel@DESKTOP-U7LANCM>
2020-05-15 19:04:28 -0300

    feat: Criação da classe Categoria

---
+++ src/br/com/connect/categoria/Categoria.java
@@ -0,0 +1,15 @@
+package br.com.connect.categoria;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Lucas Miguel
+ */
+public class Categoria {
+    Integer idCategoria;
+    String desCategoria;
+    Integer categoriaUsuario;
+ 
+    
+}

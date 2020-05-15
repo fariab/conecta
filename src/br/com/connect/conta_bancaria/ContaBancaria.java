92babf3d036dc2ecade964ff63bf0c9a1754345d
Lucas Miguel <Lucas Miguel@DESKTOP-U7LANCM>
2020-05-15 19:38:18 -0300

    feat: Criação classe ContaBancaria

---
+++ src/br/com/connect/conta_bancaria/ContaBancaria.java
@@ -0,0 +1,17 @@
+
+package br.com.connect.conta_bancaria;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Lucas Miguel
+ */
+public class ContaBancaria {
+    Integer idConta;
+    String nomBanco;
+    Integer numAgencia;
+    Integer numconta;
+
+    
+}

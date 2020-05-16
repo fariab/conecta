fc9c81fe8b4972f60e24f7c9f5de27127d29a225
Matheus <Matheus@DESKTOP-ULL36TA>
2020-05-15 21:06:59 -0300

    feat: -Encapsulamento e códigos equals, hashcode e toString inseridos na classe usuario

--- src/br/com/connect/usuario/Usuario.java
+++ src/br/com/connect/usuario/Usuario.java
@@ -5,15 +5,134 @@
  */
 package br.com.connect.usuario;
 
+import java.util.Objects;
+
 /**
  *
  * @author Matheus
  */
 public class Usuario {
-    Integer idUsuario;
-    String nomUsuario;
-    Integer numCpf;
-    String enderecoUsuario;
-    Integer telUsuario;
+    private Integer idUsuario;
+    private String nomUsuario;
+    private Integer numCpf;
+    private String enderecoUsuario;
+    private Integer telUsuario;
+
+    /**
+     * @return the idUsuario
+     */
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    /**
+     * @param idUsuario the idUsuario to set
+     */
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    /**
+     * @return the nomUsuario
+     */
+    public String getNomUsuario() {
+        return nomUsuario;
+    }
+
+    /**
+     * @param nomUsuario the nomUsuario to set
+     */
+    public void setNomUsuario(String nomUsuario) {
+        this.nomUsuario = nomUsuario;
+    }
+
+    /**
+     * @return the numCpf
+     */
+    public Integer getNumCpf() {
+        return numCpf;
+    }
+
+    /**
+     * @param numCpf the numCpf to set
+     */
+    public void setNumCpf(Integer numCpf) {
+        this.numCpf = numCpf;
+    }
+
+    /**
+     * @return the enderecoUsuario
+     */
+    public String getEnderecoUsuario() {
+        return enderecoUsuario;
+    }
+
+    /**
+     * @param enderecoUsuario the enderecoUsuario to set
+     */
+    public void setEnderecoUsuario(String enderecoUsuario) {
+        this.enderecoUsuario = enderecoUsuario;
+    }
+
+    /**
+     * @return the telUsuario
+     */
+    public Integer getTelUsuario() {
+        return telUsuario;
+    }
+
+    /**
+     * @param telUsuario the telUsuario to set
+     */
+    public void setTelUsuario(Integer telUsuario) {
+        this.telUsuario = telUsuario;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 37 * hash + Objects.hashCode(this.idUsuario);
+        hash = 37 * hash + Objects.hashCode(this.nomUsuario);
+        hash = 37 * hash + Objects.hashCode(this.numCpf);
+        hash = 37 * hash + Objects.hashCode(this.telUsuario);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Usuario other = (Usuario) obj;
+        if (!Objects.equals(this.idUsuario, other.idUsuario)) {
+            return false;
+        }
+        if (!Objects.equals(this.nomUsuario, other.nomUsuario)) {
+            return false;
+        }
+        if (!Objects.equals(this.numCpf, other.numCpf)) {
+            return false;
+        }
+        if (!Objects.equals(this.telUsuario, other.telUsuario)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "idUsuario=" + idUsuario + ", nomUsuario=" + nomUsuario + ", numCpf=" + numCpf + ", enderecoUsuario=" + enderecoUsuario + ", telUsuario=" + telUsuario + '}';
+    }
+
+    public Usuario(Integer idUsuario, String nomUsuario, Integer numCpf, String enderecoUsuario, Integer telUsuario) {
+        this.idUsuario = idUsuario;
+        this.nomUsuario = nomUsuario;
+        this.numCpf = numCpf;
+        this.enderecoUsuario = enderecoUsuario;
+        this.telUsuario = telUsuario;
+    }
     
 }
